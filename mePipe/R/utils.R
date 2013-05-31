2c94dfca630ab1b49ce262e71b953977c12ee0a2
Peter Humburg <humburg@well.ox.ac.uk>
2013-05-31 11:34:36 +0100

    Added functions to access genotypes for named SNPs in SlicedData objects and convert them to SnpMatrix objects.

---
+++ mePipe/R/utils.R
@@ -0,0 +1,17 @@
+#' Get data for a named SNP from SlicedData object
+getSNP <- function(data, snp){
+	loc <- data$FindRow(snp)
+	ans <- NULL
+	if(!is.null(loc)){
+		ans <- loc$row
+	}
+	ans
+}
+
+#' Extract named SNPs from SlicedData object and return them as aSnpMatrix
+toSnpMatrix <- function(data, snps){
+	ans <- lapply(snps, getSNP, data=data)
+	ans <- t(Reduce(rbind, ans))
+	mode(ans) <- "raw"
+	new("SnpMatrix", ans)	
+}
