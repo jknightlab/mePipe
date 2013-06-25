034d83e9a0f1882e376d6e6917389b100eefbbda
Peter Humburg <humburg@well.ox.ac.uk>
2013-06-25 12:11:12 +0100

    check relevant option when deciding which covariate file to load

--- mePipe/exec/runMatrixEQTL.R
+++ mePipe/exec/runMatrixEQTL.R
@@ -538,7 +538,7 @@ if(opt$multiPeak){
 		message("Resolving multiple peaks...")
 		allCovariates <- covariates
 		if(me$all$covariates > 0){
-			pc <- loadData(if(opt$filterpca) opt$filterout else opt$pcacov, 
+			pc <- loadData(if(opt$filtercov) opt$filterout else opt$pcacov, 
 					getOptions(sep = opt$delim, missing = opt$missing, 
 							rowskip = opt$rowskip, colskip = opt$colskip, 
 							slice = me$all$covariates))
@@ -555,7 +555,7 @@ if(opt$multiPeak){
 			message("Resolving multiple peaks for cis associations...")
 			cisCovariates <- covariates
 			if(me$cis$covariates > 0){
-				pc <- loadData(if(opt$filterpca) opt$filterout else opt$pcacov, 
+				pc <- loadData(if(opt$filtercov) opt$filterout else opt$pcacov, 
 						getOptions(sep = opt$delim, missing = opt$missing, 
 								rowskip = opt$rowskip, colskip = opt$colskip, 
 								slice = me$cis$covariates))
@@ -572,7 +572,7 @@ if(opt$multiPeak){
 			message("Resolving multiple peaks for trans-associations...")
 			transCovariates <- covariates
 			if(me$trans$covariates > 0){
-				pc <- loadData(if(opt$filterpca) opt$filterout else opt$pcacov, 
+				pc <- loadData(if(opt$filtercov) opt$filterout else opt$pcacov, 
 						getOptions(sep = opt$delim, missing = opt$missing, 
 								rowskip = opt$rowskip, colskip = opt$colskip, 
 								slice = me$trans$covariates))
